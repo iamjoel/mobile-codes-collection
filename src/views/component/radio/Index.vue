13919ed70b715b5f014de9700a5ce8b158822047
顾家伟 <328664896@qq.com>
2018-08-07 14:10:27 +0800

    fin radio

--- src/views/component/radio/Index.vue
+++ src/views/component/radio/Index.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="main">
+    <div class="demo-block">
+      <div class="label">
+        基础用法
+      </div>
+      <van-radio-group v-model="radio">
+        <van-radio name="1">单选框 1</van-radio>
+        <van-radio name="2">单选框 2</van-radio>
+      </van-radio-group>
+    </div>
+    <div class="demo-block">
+      <div class="label">
+        禁用状态
+      </div>
+      <van-radio-group v-model="radio" disabled>
+        <van-radio name="1">单选框 1</van-radio>
+        <van-radio name="2">单选框 2</van-radio>
+      </van-radio-group>
+    </div>
+    <div class="demo-block">
+      <div class="label">
+        与cell组件一起使用
+      </div>
+      <van-radio-group v-model="radio">
+        <van-cell-group>
+          <van-cell title="单选框 1" clickable @click="radio = '1'">
+            <van-radio name="1" />
+          </van-cell>
+          <van-cell title="单选框 2" clickable @click="radio = '2'">
+            <van-radio name="2" />
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </div>
   </div>
 </template>
 
@@ -7,7 +40,7 @@
 export default {
   data() {
     return {
-
+      radio: '1'
     }  
   },
   methods: {
