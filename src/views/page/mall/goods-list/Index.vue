78563f908b987543f8a98b5aac1c51ae664332dc
joel <iamjoel007@gmail.com>
2018-08-15 08:00:05 +0800

    enchance outlook

--- src/views/page/mall/goods-list/Index.vue
+++ src/views/page/mall/goods-list/Index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="main">
-    <div class="container">
-      <van-row gutter="20" v-if="true">
+    <div class="ph-10">
+      <van-row gutter="10" v-if="true">
         <van-col span="12" v-for="i in 10" :key="i">
-          <div class="media"  @click="$router.push('/page/mall/goods-detail/' + i)">
-            <img :src="'http://via.placeholder.com/100x100?text=' + i"  class="media__img"/>
-            <div class="media__name">商品名称{{i}}</div>
+          <div class="goods"  @click="$router.push('/page/mall/goods-detail/' + i)">
+            <img :src="'http://via.placeholder.com/100x100?text=' + i"  class="goods__img"/>
+            <div class="goods__info">
+              <div class="goods__name">商品{{i}}</div>
+              <div class="goods__price">￥<strong>25.6</strong></div>
+            </div>
           </div>
         </van-col>
       </van-row>
@@ -33,11 +36,23 @@ export default {
 </script>
 
 <style scoped>
-  .media__img {
+  .goods__img {
+    width: 100%;
     height: 3rem;
   }
-  .media__name {
-    text-align: center;
+  .goods__info {
     line-height: 1.5;
+    margin-bottom: .2rem;
   }
+  .goods__price {
+    font-family: arial;
+    font-size: 16px;
+    color: #f00;
+  }
+  .goods__price strong{
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+
 </style>
