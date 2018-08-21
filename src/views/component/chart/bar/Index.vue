1c0e9b95ff3786703a8331c8e1c2af338397f149
joel <iamjoel007@gmail.com>
2018-08-21 15:49:27 +0800

    enchance chart

--- src/views/component/chart/bar/Index.vue
+++ src/views/component/chart/bar/Index.vue
@@ -3,7 +3,7 @@
     <section>
       <div class="demo__title">基础用法</div>
       <div class="p-20rem" style="height:4.5rem;overflow: hidden;">
-        <!-- 减少简介，位置微调 -->
+        <!-- 减少间隙，位置微调 -->
         <div style="width: 100%;height: 6rem;position: relative;top: -1.3rem">
           <chart :options="chartOpts"></chart>
         </div>
@@ -11,6 +11,15 @@
     </section>
     
     <p>其他内容</p>
+
+    <div class="p-10rem">
+      注意：需要在 webpack.base.conf.js 中的 loader: 'babel-loader' 加 <br>
+      <code >
+        resolve('node_modules/vue-echarts'),<br>
+        resolve('node_modules/resize-detector') <br>
+      </code>
+      不加 npm run build 时会报错。
+    </div>
   </div>
 </template>
 
--- src/views/component/chart/line/Index.vue
+++ src/views/component/chart/line/Index.vue
@@ -2,11 +2,22 @@
   <div class="main">
     <section>
       <div class="demo__title">基础用法</div>
-      <div class="p-20rem" style="height:6rem">
-        <chart :options="chartOpts"></chart>
+      <div class="p-20rem" style="height:4rem">
+        <!-- 减少间隙，位置微调 -->
+        <div style="width: 100%;height: 6rem;position: relative;top: -1.3rem">
+          <chart :options="chartOpts"></chart>
+        </div>
       </div>
     </section>
     
+    <div class="p-10rem">
+      注意：需要在 webpack.base.conf.js 中的 loader: 'babel-loader' 加 <br>
+      <code >
+        resolve('node_modules/vue-echarts'),<br>
+        resolve('node_modules/resize-detector') <br>
+      </code>
+      不加 npm run build 时会报错。
+    </div>
   </div>
 </template>
 
