8fcb2ccf4ed12de19bed1a6dcd73634ace319434
joel <iamjoel007@gmail.com>
2018-08-24 00:46:08 +0800

    enchance field

--- src/views/component/form/field/Index.vue
+++ src/views/component/form/field/Index.vue
@@ -1,44 +1,35 @@
 <template>
   <div class="main">
-    <div class="demo-block">
+    <section class="demo">
       <div class="demo__title">
         基础用法
       </div>
       <van-cell-group>
-        <van-field v-model="value" placeholder="请输入用户名" />
+        <van-field placeholder="请输入用户名" />
       </van-cell-group>
-      <br>
+      
       <van-cell-group>
-        <van-field v-model="value" type="textarea" placeholder="请输入用户名" />
+        <van-field v-model="value" type="textarea" placeholder="请输入描述" />
       </van-cell-group>
-    </div>
-    <div class="demo-block">
+      <div class="mt-5">只适合表单项少的情况用。表单项多时，用这种方式，用户填完表单会不知道表单对于的内容</div>
+    </section>
+
+    <section class="demo">
       <div class="demo__title">
-        输入框只读
+        带标签的表单
       </div>
       <van-cell-group>
-        <van-field v-model="value" readonly placeholder="请输入用户名" />
+        <van-field placeholder="请输入用户名" label="用户名"/>
       </van-cell-group>
-      <br>
+      
       <van-cell-group>
-        <van-field v-model="value" type="textarea" readonly placeholder="请输入用户名" />
+        <van-field v-model="value" type="textarea" placeholder="请输入描述" label="描述" />
       </van-cell-group>
-    </div>
-    <div class="demo-block">
+    </section>
+
+    <section class="demo">
       <div class="demo__title">
-        禁用输入框
-      </div>
-      <van-cell-group>
-        <van-field v-model="value" disabled placeholder="请输入用户名" />
-      </van-cell-group>
-      <br>
-      <van-cell-group>
-        <van-field v-model="value" type="textarea" disabled placeholder="请输入用户名" />
-      </van-cell-group>
-    </div>
-    <div class="demo-block">
-      <div class="demo__title">
-        自定义输入框
+        必填项&带清空按钮
       </div>
       <van-cell-group>
         <van-field
@@ -57,10 +48,31 @@
           required
         />
       </van-cell-group>
-    </div>
-    <div class="demo-block">
+    </section>
+
+    <section class="demo">
       <div class="demo__title">
-        自适应高度
+        输入框只读
+      </div>
+      <van-cell-group>
+        <van-field v-model="value" readonly placeholder="请输入用户名" label="用户名"/>
+      </van-cell-group>
+    </section>
+
+    <section class="demo">
+      <div class="demo__title">
+        禁用输入框
+      </div>
+      <van-cell-group>
+        <van-field v-model="value" disabled placeholder="请输入用户名" label="用户名"/>
+      </van-cell-group>
+    </section>
+
+    
+
+    <section class="demo">
+      <div class="demo__title">
+        多行文本自适应高度
       </div>
       <van-cell-group>
         <van-field
@@ -72,7 +84,7 @@
           autosize
         />
       </van-cell-group>
-    </div>
+    </section>
   </div>
 </template>
 
