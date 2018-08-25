b85ae0f001eba2acc74837104188d2ecb3c463d6
joel <iamjoel007@gmail.com>
2018-08-25 22:42:11 +0800

    enchance slide

--- src/views/component/form/Slide.vue
+++ src/views/component/form/Slide.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="main">
-    <h1>需要安装 vue-slider-component</h1>
-    <div class="demo__title">基础用法</div>
-    <div class="ly ly-m p-20rem">
-      音量值:
-      <div style="flex-grow: 1">
-         <vue-slider v-model="value1"></vue-slider>
+    <h1 class="ff-yahei">van-slider</h1>
+    <section class="demo">
+      <h2 class="demo__title">基础用法</h2>
+      <div class="mv-10">van-slider 只支持选择范围为：0~100</div>
+      <van-slider 
+        v-model="slideValue" 
+        :min="10"
+        :max="90"
+        :step="5"
+      />
+      <div class="mv-10">当前值: {{slideValue}}</div>
+    </section>
+    <h1 class="ff-yahei" style="margin-top: 50px;">vue-slider-component</h1>
+    <div class="m-20">vue-slider-component 支持更多的功能</div>
+    <section class="demo">
+      <h2 class="demo__title">基础用法</h2>
+        <div class="ly ly-m p-20rem">
+          音量值:
+          <div style="flex-grow: 1">
+             <vue-slider v-model="value1"></vue-slider>
+          </div>
       </div>
-    </div>
-
-    <div class="demo__title">范围</div>
-    <div class="ly ly-m p-20rem">
-      价格范围:
-      <!-- 
-        interval: 间距
-        piecewise: 画价格点
-       -->
-      <div style="flex-grow: 1">
-         <vue-slider 
-          v-model="value2"
-          :min="10"
-          :max="100"
-          :interval="10"
-          piecewise
-          ></vue-slider>
+    </section>
+    
+    <section class="demo">
+      <h2 class="demo__title">范围</h2>
+      <div class="ly ly-m p-20rem">
+        价格范围:
+        <!-- 
+          interval: 间距
+          piecewise: 画价格点
+         -->
+        <div style="flex-grow: 1">
+           <vue-slider 
+            v-model="value2"
+            :min="10"
+            :max="100"
+            :interval="10"
+            piecewise
+            ></vue-slider>
+        </div>
       </div>
-    </div>
+    </section>
+    
     
   </div>
 </template>
@@ -39,6 +57,7 @@ export default {
   },
   data() {
     return {
+      slideValue: 20,
       value1: 5,
       value2: [20, 50],
     }  
@@ -49,4 +68,11 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin: .1rem 0;
+  padding: 0 .2rem;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
