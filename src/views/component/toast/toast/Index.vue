f13d5867d28ba6232f3c9dc58f1cceb91508bc2f
joel <iamjoel007@gmail.com>
2018-08-26 19:53:04 +0800

    enchance toast

--- src/views/component/toast/toast/Index.vue
+++ src/views/component/toast/toast/Index.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="main">
-    <div class="demo__title">文字提示</div>
-    <div class="p-10 b d-ib ml-15" @click="onClickToast">文字提示</div>
+    <section class="demo">
+      <h2 class="demo__title">基础用法</h2>
+      <van-button @click="$toast('我是提示文案，建议不超过十五字~')">显示轻提示</van-button>
+    </section>
+
+    <section class="demo">
+      <h2 class="demo__title">成功提示</h2>
+      <van-button @click="$toast.success('操作成功')">显示成功提示</van-button>
+    </section>
+
+    <section class="demo">
+      <h2 class="demo__title">失败提示</h2>
+      <van-button @click="$toast.fail('操作失败')">显示失败提示</van-button>
+    </section>
+
+     <section class="demo">
+      <h2 class="demo__title">关闭提示</h2>
+      <code>$toast.clear()</code>
+    </section>
+
+    <section class="demo">
+      <h2 class="demo__title">各种配置</h2>
+      <van-button @click="$toast({
+        message: '请等待5秒...',
+        type: 'loading',
+        loadingType: 'spinner',
+        duration: 5000, // 展示时长
+        mask: true,  //显示背景蒙层
+        forbidClick: true,
+      })">各种配置</van-button>
+    </section>
+
+
   </div>
 </template>
 
@@ -12,9 +43,7 @@ export default {
     }  
   },
   methods: {
-    onClickToast() {
-      this.$toast('我是提示文案，建议不超过十五字~')
-    }
+    
   }
 }
 </script>
