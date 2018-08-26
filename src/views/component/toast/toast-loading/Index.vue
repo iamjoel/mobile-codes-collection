56db58b6d61db40233e4593e2b7efc8072a119b5
joel <iamjoel007@gmail.com>
2018-08-26 20:12:50 +0800

    enchance loading

--- src/views/component/toast/toast-loading/Index.vue
+++ src/views/component/toast/toast-loading/Index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="main">
-    <div class="demo__title">加载提示</div>
-    <div class="p-10 b d-ib ml-15" @click="onClickToast">加载提示</div>
+    <section class="demo">
+      <h2 class="demo__title">基础用法</h2>
+      <van-button @click="showBasicLoading">显示加载中</van-button>
+    </section>
+
+    <section class="demo">
+      <h2 class="demo__title">加载图标是 spinng</h2>
+      <van-button @click="$toast.loading({
+        loadingType: 'spinner',
+        mask: true,
+        message: '加载中...'
+      })">显示加载中</van-button>
+    </section>
   </div>
 </template>
 
@@ -12,11 +23,16 @@ export default {
     }  
   },
   methods: {
-    onClickToast() {
-      this.$toast.loading({
+    showBasicLoading() {
+      var toast = this.$toast.loading({
         mask: true,
-        message: '加载中...'
+        message: '加载中...',
+        duration: 0, // 加载不会自动消失
       })
+      // 模拟关闭
+      setTimeout(()=> {
+        toast.clear()
+      }, 5000)
     }
   }
 }
