a328bfe5a17d0d85e86966db6ab1e4385a942569
顾家伟 <328664896@qq.com>
2018-08-31 16:22:41 +0800

    enchance

--- src/views/tool/other/img/Index.vue
+++ src/views/tool/other/img/Index.vue
@@ -17,6 +17,14 @@
       <div class="demo__title">暂无图片</div>
       <img src="./images/no-pic.gif" alt="" class="w-100per">
     </section>
+
+    <section>
+      <div class="demo__title">404</div>
+      <div class="ta-c">
+        <img src="./images/404.png" alt="" class="w-100per">
+        <div class="error-text ff-yahei">找不到该页面!</div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -44,5 +52,11 @@ export default {
     width: 1rem;
     height: 1rem;
     border-radius: 50%;
+  }
+
+  .error-text {
+    margin-top: .1rem;
+    font-size: 16px;
+    color: #666;
   }
 </style>
