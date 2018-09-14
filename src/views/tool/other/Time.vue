456e06a6e59bf8f6a1231608600042ae555dab14
joel <iamjoel007@gmail.com>
2018-09-14 18:22:37 +0800

    add calute month

--- src/views/tool/other/Time.vue
+++ src/views/tool/other/Time.vue
@@ -1,17 +1,29 @@
 
 <template>
   <div class="main">
-    <div class="demo__title">时间格式化</div>
-    YYYY-MM-DD : {{Date.now() | time}} <br>
-    YY年MM月DD日 : {{Date.now() | time('YY年MM月DD日')}} <br>
-    YYYY/MM/DD HH:mm: ss: {{Date.now() | time('YYYY/MM/DD HH:mm: ss')}}
+    <section class="demo">
+      <div class="demo__title">时间格式化</div>
+      YYYY-MM-DD : {{Date.now() | time}} <br>
+      YY年MM月DD日 : {{Date.now() | time('YY年MM月DD日')}} <br>
+      YYYY/MM/DD HH:mm: ss: {{Date.now() | time('YYYY/MM/DD HH:mm: ss')}}
+    </section>
+    
+    <section class="demo">
+      <div class="demo__title">距当前时间</div>
+      {{justNow | time('YYYY/MM/DD HH:mm')}} 是 {{justNow | diffNow}} <br>
+      {{minutesAgo | time('YYYY/MM/DD HH:mm')}} 是 {{minutesAgo | diffNow}} <br>
+      {{hoursAgo | time('YYYY/MM/DD HH:mm')}} 是 {{hoursAgo | diffNow}} <br>
+      {{daysAgo | time('YYYY/MM/DD HH:mm')}} 是 {{daysAgo | diffNow}} <br>
+      {{longAgo | time('YYYY/MM/DD HH:mm')}} 是 {{longAgo | diffNow(undefined, 'YY年MM月DD日')}} <br>
+    </section>
 
-    <div class="demo__title">距当前时间</div>
-    {{justNow | time('YYYY/MM/DD HH:mm')}} 是 {{justNow | diffNow}} <br>
-    {{minutesAgo | time('YYYY/MM/DD HH:mm')}} 是 {{minutesAgo | diffNow}} <br>
-    {{hoursAgo | time('YYYY/MM/DD HH:mm')}} 是 {{hoursAgo | diffNow}} <br>
-    {{daysAgo | time('YYYY/MM/DD HH:mm')}} 是 {{daysAgo | diffNow}} <br>
-    {{longAgo | time('YYYY/MM/DD HH:mm')}} 是 {{longAgo | diffNow(undefined, 'YY年MM月DD日')}} <br>
+    <section class="demo">
+      <div class="demo__title">一个月有多少天</div>
+      2018年1月有 {{getMonthDay('2018/1/1')}}天 <br>
+      2018年2月有 {{getMonthDay('2018/2/1')}}天 <br>
+      2020年2月有 {{getMonthDay('2020/2/1')}}天
+    </section>
+    
   </div>
 </template>
 
@@ -28,8 +40,16 @@ export default {
     }
   },
   methods: {
-    
+    getMonthDay
   }
+}
+
+// 一个月有多少天
+function getMonthDay(date) {
+  date = moment(date).toDate()
+  date.setMonth(date.getMonth() + 1)
+  date.setDate(0) // 这个月最后一天
+  return date.getDate()
 }
 </script>
 
