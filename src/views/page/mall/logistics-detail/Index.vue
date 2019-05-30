771e40295152772193caad87917ad3209ce91f00
jin <731568065@qq.com>
2019-05-30 17:39:16 +0800

    修改物流详情页面

--- src/views/page/mall/logistics-detail/Index.vue
+++ src/views/page/mall/logistics-detail/Index.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="logistics-detail">
     <ul class="logistics-detail__item">
-      <li class="ly ly-m item">
+      <li class="ly  item">
         <div class="item__time ta-r">
           <div>12-08<br>
           22:21</div>
         </div>
-        <i class="item__icon"></i>
         <div class="item__content">苏州转运中心 已收入</div>
       </li>
-      <li class="ly ly-m item">
+      <li class="ly  item">
         <div class="item__time ta-r">
           <div>12-08<br>
             20:25</div>
         </div>
-        <i class="item__icon"></i>
         <div class="item__content">无锡转运中心公司 已发出，下一站 苏州转运中心</div>
       </li>
-      <li class="ly ly-m item">
+      <li class="ly  item">
         <div class="item__time ta-r">
           <div>12-08<br>
           00:01</div>
         </div>
-        <i class="item__icon"></i>
-        <div class="item__content">无锡转运动型 已经收入</div>
+        <div class="item__content item__content--last">无锡转运动型 已经收入</div>
       </li>
     </ul>
   </div>
